a5895b11446bfa312549477b12214c51bfb03fee
Wenxin Li <li000594@algonquinlive.com>
2024-03-31 19:34:45 -0400

    surplusFood

---
+++ src/java/dao/SurplusFoodDao.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package dao;
+
+import entity.SurplusFood;
+import java.util.List;
+
+public interface SurplusFoodDao {
+    void addSurplusFood(SurplusFood food);
+    void updateSurplusFood(SurplusFood food);
+    void deleteSurplusFood(int foodId);
+    SurplusFood getSurplusFoodById(int foodId);
+    List<SurplusFood> getAllSurplusFoods();
+    List<SurplusFood> getSurplusFoodsForDonation();
+    List<SurplusFood> getSurplusFoodsForSale();
+}
